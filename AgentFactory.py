2042405f11293e4ad485ebb4202f0656f3d91168
Clay O'Neil <oneil512@umn.edu>
2021-03-13 11:29:42 -0600

    fix dependency issues

--- AgentFactory.py
+++
@@ -1,2 +0,0 @@
-class AgentFactory:
-    pass
--- MarketSimulation.py
+++ MarketSimulation.py
@@ -1,12 +1,4 @@
-#simulate a market
-#n participants
-#each has cash reserve
-#all try to take profit at some point
-#agents should try to maximize profit
-#how does the market start?
-#risk propensity
-#agents try to identify trends and exploit them based on risk, higher risk = lower time frame
-#trends are defined by increasing/decreasing moving averages, higher risk changes moving average time preferance
-#market ticks every second
+import src.Agent
+
 
 
